de931a818f3e0aba632ec218e69b2fd0ee3d20b0
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2016-07-26 15:01:09 +0200

    structs file created

---
+++ Visualizer/Visualizer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Fabian on 26.07.2016.
+//
+
+#include "Visualizer.h"
---
+++ Visualizer/Visualizer.h
@@ -0,0 +1,8 @@
+//
+// Created by Fabian on 26.07.2016.
+//
+
+#ifndef GRP04_ALGDATII_VISUALIZER_H
+#define GRP04_ALGDATII_VISUALIZER_H
+
+#endif //GRP04_ALGDATII_VISUALIZER_H
