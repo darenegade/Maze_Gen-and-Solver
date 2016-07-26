c7e43ef0358b2399da084053a2f3da14366191f8
Arne <arne.schoentag@imagefiguren.de>
2016-07-26 15:30:17 +0200

    generator stand dienstag(ich muss jetzt los)

---
+++ src/LabGenerator/Generator.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Arne on 26.07.2016.
+//
+
+#include <vector>
+#include "/Structs/Structs.h"
+using namespace std;
+
+static const int HEIGHT = 12;
+static const int WIDTH = 12;
+
+vector<vector<bool>> getDefaultLabirith(){
+    Maze maze = new Maze(HEIGHT,WIDTH);
+    int test[HEIGHT][WIDTH] = {{1,1,1,1,1,0,1,1,1,1,1,1},
+                               {1,0,0,0,0,0,0,0,0,0,0,1},
+                               {1,0,1,1,1,1,1,1,1,0,1,1},
+                               {1,0,1,0,0,0,0,0,1,0,0,1},
+                               {1,0,1,0,1,1,1,0,1,1,0,1},
+                               {1,0,0,0,1,0,0,0,1,0,0,1},
+                               {1,1,1,1,1,1,1,0,1,0,1,1},
+                               {1,0,0,0,0,0,1,0,1,1,1,1},
+                               {1,0,1,0,1,0,0,0,0,1,0,1},
+                               {1,0,1,0,1,1,1,0,1,1,0,1},
+                               {1,0,1,0,0,0,0,0,0,0,0,1},
+                               {1,1,1,0,1,1,1,1,1,1,1,1}};
+    for(int y; y < HEIGHT; y++){
+        for(int x; x < WIDTH; x++){
+//            Maze
+        }
+    }
+
+
+}
+
+
