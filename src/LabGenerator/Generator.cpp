60a15650b359fa23d7db1d24f3cfec8755237f0c
Arne <arne.schoentag@imagefiguren.de>
2016-07-27 09:43:58 +0200

    one step to the future

--- src/LabGenerator/Generator.cpp
+++ src/LabGenerator/Generator.cpp
@@ -2,15 +2,15 @@
 // Created by Arne on 26.07.2016.
 //
 
-#include <vector>
-#include "/Structs/Structs.h"
+#include <iostream>
+#include "../Structs/Structs.h"
 using namespace std;
 
 static const int HEIGHT = 12;
 static const int WIDTH = 12;
 
-vector<vector<bool>> getDefaultLabirith(){
-    Maze maze = new Maze(HEIGHT,WIDTH);
+Maze * getDefaultLabirith(){
+    Maze * maze = new Maze(HEIGHT,WIDTH);
     int test[HEIGHT][WIDTH] = {{1,1,1,1,1,0,1,1,1,1,1,1},
                                {1,0,0,0,0,0,0,0,0,0,0,1},
                                {1,0,1,1,1,1,1,1,1,0,1,1},
@@ -25,11 +25,17 @@ vector<vector<bool>> getDefaultLabirith(){
                                {1,1,1,0,1,1,1,1,1,1,1,1}};
     for(int y; y < HEIGHT; y++){
         for(int x; x < WIDTH; x++){
-//            Maze
+            Maze::Coordinate *coord = new Maze::Coordinate(x, y);
+            maze->setPosition(coord, test[y][x]);
         }
     }
+    return maze;
+}
 
+int main() {
+    cout << "hello" << endl;
 
+    return 0;
 }
 
 
--- src/Structs/Structs.h
+++ src/Structs/Structs.h
@@ -15,11 +15,11 @@ private:
     int height;
     int width;
 
-    Maze(int height, int width): height(height), width(width) {
+public:
+
+    Maze(const int height,const int width): height(height), width(width) {
         maze = new vector(height, new vector<bool>(width));
     }
-
-public:
 
     struct Coordinate {
         int x;
@@ -28,12 +28,12 @@ public:
         Coordinate(int x, int y) : x(x), y(y) {}
     };
 
-    bool getPosition(const Coordinate coordinate) {
-        return maze->at(coordinate.y)->at(coordinate.x);
+    bool getPosition(Coordinate * coordinate) {
+        return maze->at(coordinate->y)->at(coordinate->x);
     }
 
-    void setPosition(const  Coordinate coordinate, bool value) {
-        maze->at(coordinate.y)->at(coordinate.x) = value;
+    void setPosition(Coordinate * coordinate, bool value) {
+        maze->at(coordinate->y)->at(coordinate->x) = value;
     }
 
     int getHeight(){
