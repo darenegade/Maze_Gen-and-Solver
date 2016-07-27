a33d3603ac1837adf97a470c229ce28ed43f0de6
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2016-07-27 14:42:47 +0200

    drawing stuff

--- src/Visualizer/Visualizer.cpp
+++ src/Visualizer/Visualizer.cpp
@@ -2,53 +2,126 @@
 // Created by Fabian on 26.07.2016.
 //
 
+#include <iostream>
 #include "Visualizer.h"
 
-void Visualizer::visualize() {
-    //The window we'll be rendering to
-    SDL_Window *window = NULL;
+using namespace std;
 
-    //The surface contained by the window
-    SDL_Surface *screenSurface = NULL;
+void Visualizer::visualize() {
+    if (!init()) {
+        printf("Error initializing Windows");
+        exit(EXIT_FAILURE);
+    };
+
+    drawWalls();
+    drawWays();
+    drawDoors();
+    drawPath();
+
+    //Update the surface
+    SDL_UpdateWindowSurface(gWindow);
+    SDL_Delay(2000);
+    close();
+}
+
+bool Visualizer::init() {
+    //Initialization flag
+    bool success = true;
 
     //Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    } else {
+        success = false;
+    }
+    else {
         //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SCREEN_POS_X, SCREEN_POS_Y, SCREEN_WIDTH,
-                                  SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
+        gWindow = SDL_CreateWindow("Maze Solver",
+                                   SCREEN_POS_X,
+                                   SCREEN_POS_Y,
+                                   SCREEN_WIDTH,
+                                   SCREEN_HEIGHT,
+                                   SDL_WINDOW_SHOWN);
+        if (gWindow == NULL) {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
         }
         else {
             //Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
-
-            //Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-            //Update the surface
-            SDL_UpdateWindowSurface(window);
-
-            //Wait two seconds
-            SDL_Delay(2000);
-        }//Destroy window
-        SDL_DestroyWindow(window);
-
-        //Quit SDL subsystems
-        SDL_Quit();
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
     }
-}
 
-bool Visualizer::init() {
-    return false;
-}
-
-bool Visualizer::loadMedia() {
-    return false;
+    return success;
 }
 
 void Visualizer::close() {
+
+    //Destroy window
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
+
+void Visualizer::drawWays(Maze *maze) {
+    SDL_Rect rect = *new SDL_Rect();
+    rect.h = ENTITY_SIZE;
+    rect.w = ENTITY_SIZE;
+    for (int i = 0; i < maze->getHeight(); i++) {
+        for (int j = 0; j < maze->getWidth(); j++) {
+            if (!(maze->getPosition(i, j))) {
+                rect.x = i * ENTITY_SIZE;
+                rect.y = j * ENTITY_SIZE;
+                SDL_FillRect(gScreenSurface,
+                             &rect,
+                             cWay);
+            }
+        }
+    }
+}
+
+void Visualizer::drawWalls(Maze *maze) {
+    SDL_Rect rect = *new SDL_Rect();
+    rect.h = ENTITY_SIZE;
+    rect.w = ENTITY_SIZE;
+    for (int i = 0; i < maze->getHeight(); i++) {
+        for (int j = 0; j < maze->getWidth(); j++) {
+            if (maze->getPosition(i, j)) {
+                rect.x = i * ENTITY_SIZE;
+                rect.y = j * ENTITY_SIZE;
+                SDL_FillRect(gScreenSurface,
+                             &rect,
+                             cWall);
+            }
+        }
+    }
+}
+
+void Visualizer::drawDoors(Maze::Coordinate *entry, Maze::Coordinate *exit) {
+    SDL_Rect rect = *new SDL_Rect();
+    rect.h = ENTITY_SIZE;
+    rect.w = ENTITY_SIZE;
+
+    rect.x = entry->x;
+    rect.y = entry->y;
+    SDL_FillRect(gScreenSurface, &rect, cDoor);
+    rect.x = exit->x;
+    rect.y = exit->y;
+    SDL_FillRect(gScreenSurface, &rect, cDoor);
+}
+
+void Visualizer::drawPath(list<Maze::Coordinate> coords) {
+    SDL_Rect rect = *new SDL_Rect();
+    rect.h = ENTITY_SIZE;
+    rect.w = ENTITY_SIZE;
+    Maze::Coordinate *coordinate;
+    for (list<Maze::Coordinate>::iterator iterator = coords.begin(), end = coords.end(); iterator != end; ++iterator) {
+        coordinate = &*iterator;
+        rect.x = coordinate->x;
+        rect.y = coordinate->y;
+        SDL_FillRect(gScreenSurface, &rect, cPath);
+    }
+
 
 }
--- src/Visualizer/Visualizer.h
+++ src/Visualizer/Visualizer.h
@@ -8,23 +8,52 @@
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include <list>
+#include "../Structs/Structs.h"
 
 class Visualizer {
 
 private:
-    //Screen dimension constants
-    int SCREEN_WIDTH = 640;
-    int SCREEN_HEIGHT = 480;
+    //The window we'll be rendering to
+    SDL_Window *gWindow = NULL;
+
+    //The surface contained by the window
+    SDL_Surface *gScreenSurface = NULL;
+
+    //Screen dimension and position constants
+    int SCREEN_WIDTH = 1280;
+    int SCREEN_HEIGHT = 1280;
     const int SCREEN_POS_X = 50;
     const int SCREEN_POS_Y = 50;
 
+    // Entity Dimensions
+    const int ENTITY_SIZE = 30;
+    // Wall Color
+    Uint32 cWall = SDL_MapRGBA(gScreenSurface->format, 153, 0, 0, 255);
+    // Color of free paths
+    Uint32 cWay = SDL_MapRGBA(gScreenSurface->format, 224, 224, 224, 255);
+    // Color of entry and exit
+    Uint32 cDoor = SDL_MapRGBA(gScreenSurface->format, 0, 0, 255, 255);
+    // Color of the calculated path
+    Uint32 cPath = SDL_MapRGBA(gScreenSurface->format, 102, 255, 102, 255);
+
     bool init();
-    bool loadMedia();
+
+    void drawWalls(Maze *maze);
+
+    void drawWays(Maze *maze);
+
+    void drawDoors(Maze::Coordinate *entry, Maze::Coordinate *exit);
+
+    void drawPath(list<Maze::Coordinate> coords);
+
     void close();
 
 public:
-    Visualizer(){};
-    Visualizer(int width, int height): SCREEN_WIDTH(width), SCREEN_HEIGHT(height){}
+    Visualizer() {};
+
+    Visualizer(int width, int height) : SCREEN_WIDTH(width), SCREEN_HEIGHT(height) {}
+
     void visualize();
 };
 
