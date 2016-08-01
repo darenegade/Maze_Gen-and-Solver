bcebd9e6f34abb97000459c2693db4dee7aacc0e
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2016-08-01 16:01:16 +0200

    no more loop in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,22 @@
 #endif /* main */
 
 int main() {
-    list<Maze::Coordinate> way = {};
     Maze::Coordinate *start;
     Maze::Coordinate *end;
-    Maze *m = getRandomMaze();
-    Backtracker *bt;
-    while (way.empty()) {
-        delete end, start, m, way, bt;
-        m = getRandomMaze();
 
-        for (int i = 0; i < m->getWidth(); i++) {
-            if (!m->getPosition(i, 0)) {
-                start = new Maze::Coordinate(i, 0);
-            };
-            if (!m->getPosition(i, m->getHeight() - 1)) {
-                end = new Maze::Coordinate(i, m->getHeight() - 1);
-            };
-        }
-        bt = new Backtracker(m);
-        way = bt->solve(start, end);
+    Maze *m = getRandomMaze();
+    for (int i = 0; i < m->getWidth(); i++) {
+        if (!m->getPosition(i, 0)) {
+            start = new Maze::Coordinate(i, 0);
+        };
+        if (!m->getPosition(i, m->getHeight() - 1)) {
+            end = new Maze::Coordinate(i, m->getHeight() - 1);
+        };
     }
+
+    Backtracker *bt = new Backtracker(m);
+    list<Maze::Coordinate> way = bt->solve(start, end);
+
     Visualizer visualizer = *new Visualizer(m, &way, start, end);
     visualizer.visualize();
 
