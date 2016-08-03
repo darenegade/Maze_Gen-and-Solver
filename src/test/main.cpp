3c7bfc8292ed97118746e21b0144324493beac4a
peter-mueller <peter7995@gmail.com>
2016-08-03 11:16:16 +0200

    test

---
+++ src/test/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by peter-mueller on 02.08.2016.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv)
+
+{
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+}
