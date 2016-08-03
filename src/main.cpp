5e39bbc633ceb975cbdf1ae9a3071785636570a4
Max@gmail.com <Max@gmail.com>
2016-08-03 10:25:12 +0200

    merge

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #endif /* main */
 
 int main() {
-    Maze *m = getRandomMazeWithDivision(300);
+    Maze *m = getRandomMazeWithDivision(300, 300);
 
     Visualizer visualizer = *new Visualizer(m);
     visualizer.visualize();
