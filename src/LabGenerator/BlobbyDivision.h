3292e28890e1b55fee11dce463dcb8bcdfecd29a
René Zarwel <darenegade@gmx.de>
2016-08-03 12:20:39 +0200

    Doc Blobby

--- src/LabGenerator/BlobbyDivision.h
+++ src/LabGenerator/BlobbyDivision.h
@@ -11,26 +11,58 @@
 class BlobbyDivision {
 
 private:
+
+    /**
+     * A Cell of a Maze.
+     */
     struct Cell {
         const static int UNSET = 0;
         const static int A = 1;
         const static int B = 2;
 
-        int x,y;
+        int x, y;
         int state = UNSET;
         bool rightWall = false, downWall = false;
-        Cell(int x, int y){
+
+        Cell(int x, int y) {
             this->x = x;
             this->y = y;
         }
     };
 
-    static void generate(list<Cell*> *region, vector<vector<Cell>> *maze);
-    static list<Cell*>* getFreeNeighbours(Cell* cell, list<Cell*> *region, vector<vector<Cell>> *maze);
-    static void setWalls(list<Cell*> *a,list<Cell*> *b, vector<vector<Cell>> *maze);
+    /**
+     * Recursive Blobby Devision Generator.
+     *
+     * @param region Cells to use
+     * @param maze the whole maze
+     */
+    static void generate(list<Cell *> *region, vector<vector<Cell>> *maze);
+
+    /**
+     * Get the free neighbours of a cell.
+     * @param cell to use
+     * @param region to use
+     * @param maze the whole maze
+     * @return all free neighbours
+     */
+    static list<Cell *> *getFreeNeighbours(Cell *cell, list<Cell *> *region, vector<vector<Cell>> *maze);
+
+    /**
+     * Sets the walls between a and b.
+     * @param a region A
+     * @param b region B
+     * @param maze the whole maze
+     */
+    static void setWalls(list<Cell *> *a, list<Cell *> *b, vector<vector<Cell>> *maze);
 
 public:
-    static Maze* generate(unsigned int n);
+
+    /**
+     * Generates a maze.
+     * @param n size of Maze
+     * @return Maze
+     */
+    static Maze *generate(unsigned int n);
 
 };
 
